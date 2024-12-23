1e75f50e932c9eb578824f38ea554bc668f9ec22
JC <jcsmileyjr@gmail.com>
2024-12-22 20:34:27 -0600

    Bug: Fix the custom colors in the config not working.

--- src/app/components/letterBox/LetterBox.tsx
+++ src/app/components/letterBox/LetterBox.tsx
@@ -1,9 +1,10 @@
 
 
+// Use React state to persist the selected letter styling
 export default function LetterBox({letter}: {letter: string}) {
     return (
-        <div className="w-1/4 h-16 flex items-center justify-center text-primaryBlue text-3xl border border-contrastPurple rounded">
+        <button className="w-1/4 h-16 flex items-center justify-center text-primaryBlue text-3xl border-2 active:border-4 border-contrastPurple rounded focus:outline-none focus:border-secondaryBlue active:border-borderStrawberry transition duration-300">
             {letter}
-        </div>
+        </button>
     );
 }
