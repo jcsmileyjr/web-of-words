21ea227d27782c90ca6185a297d4acd2d1876ae6
JC <jcsmileyjr@gmail.com>
2024-12-22 19:42:32 -0600

    Style App: Add responsiveness to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col px-8 py-4 h-screen w-full ">
       <div className="absolute inset-0 bg-spider-pattern bg-cover bg-center opacity-20 brightness-50 contrast-50"></div>
-      <main className="flex flex-col relative z-10">
+      <main className="flex flex-col relative z-10 md:w-1/2 md:mx-auto">
         <Navbar />
 
       </main>
