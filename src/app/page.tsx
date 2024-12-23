ffef85b999b09514ac0f3af4378d27e0bc0b9ed9
JC <jcsmileyjr@gmail.com>
2024-12-22 19:29:48 -0600

    Style Main Page: Add a background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@ import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col px-8 py-4">
-      <Navbar />
-      "Web of Words" setup in progess
+    <div className="flex flex-col px-8 py-4 h-screen w-full ">
+      <div className="absolute inset-0 bg-spider-pattern bg-cover bg-center opacity-20 brightness-50 contrast-50"></div>
+      <main className="flex flex-col relative z-10">
+        <Navbar />
+
+      </main>
+      
     </div>  
   );
 }
