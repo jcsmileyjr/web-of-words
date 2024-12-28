4011bb9ee57b3beb97c0468525c265d9c1a8510c
JC <jcsmileyjr@gmail.com>
2024-12-28 14:47:03 -0600

    Style App: Add whitespace round elements and remove un-needed element

--- src/app/components/answers/Answer.tsx
+++ src/app/components/answers/Answer.tsx
@@ -5,7 +5,7 @@ export default function Answer() {
     const inputAnswers: InputAnswer[] = [{value: "She", points: 1, id: 1}, {value: "The", points: 1, id: 2}, {value: "Thread", points: 6, id: 3}, {value: "Head", points: 1, id: 4}, {value: "Read", points: 1, id: 5}, {value: "Here", points: 1, id: 6}, {value: "Trees", points: 5, id: 7}, {value: "Rat", points: 1, id: 8}, {value: "Are", points: 1, id: 9},];
     return (
         <section>
-            <h2 className="font-medium text-xl text-center text-primaryBlue mb-6">Answers</h2>
+            <h2 className="font-medium text-xl text-center text-primaryBlue my-6 underline">Answers</h2>
             <div className="flex flex-row flex-wrap justify-between gap-2">
                 {
                     inputAnswers.map((answer) => {
